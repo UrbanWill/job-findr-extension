384544306bb37eeb8c79e56bc44bd119b320f42b
WillUrban <will.urban.dev@gmail.com>
2024-03-25 15:05:24 +0700

    task: added twind to side-panel

--- src/pages/content/ui/app.tsx
+++ src/pages/content/ui/app.tsx
@@ -1,9 +1,3 @@
-import { useEffect } from 'react';
-
 export default function App() {
-  useEffect(() => {
-    console.log('content view loaded');
-  }, []);
-
-  return <div className="bg-red-500 p-2 fixed right-0 bottom-0 z-50">content view</div>;
+  return null;
 }
--- src/pages/sidepanel/index.tsx
+++ src/pages/sidepanel/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import '@pages/sidepanel/index.css';
 import refreshOnUpdate from 'virtual:reload-on-update-in-view';
 import SidePanel from '@pages/sidepanel/SidePanel';
+import { attachTwindStyle } from '@src/shared/style/twind';
 
 refreshOnUpdate('pages/sidepanel');
 
@@ -11,6 +12,7 @@ function init() {
   if (!appContainer) {
     throw new Error('Can not find #app-container');
   }
+  attachTwindStyle(appContainer, document);
   const root = createRoot(appContainer);
   root.render(<SidePanel />);
 }
