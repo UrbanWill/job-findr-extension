cdd371362c99374dd53e7d60534137db916343f1
WillUrban <will.urban.dev@gmail.com>
2024-03-31 18:49:18 +0700

    feat: success screen basic styles

--- src/pages/sidepanel/components/create-job-application-success.tsx
+++ src/pages/sidepanel/components/create-job-application-success.tsx
@@ -2,13 +2,17 @@ import { Button } from '@/components/ui/button';
 
 export default function CreateJobApplicationSuccess() {
   return (
-    <div className="flex flex-col min-h-full flex-grow space-y-6 justify-center items-center">
-      <h1>Job application saved successfully to:</h1>
-      <div className="text-2xl font-semibold tracking-wide">Placeholder</div>
-      <Button onClick={() => {}}>View in Job Findr!</Button>
-      <Button onClick={window.close} className="">
-        Close
-      </Button>
+    <div className="flex flex-col min-h-full">
+      <div className="flex-grow flex flex-col space-y-6 justify-center items-center">
+        <h1>Job application saved successfully to:</h1>
+        <div className="text-2xl font-semibold tracking-wide">Placeholder</div>
+        <Button onClick={() => {}}>View in Job Findr!</Button>
+      </div>
+      <div className="flex justify-end p-4">
+        <Button onClick={window.close} className="">
+          Close
+        </Button>
+      </div>
     </div>
   );
 }
